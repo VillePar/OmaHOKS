28f0089ea819a105a90edb6ddf0b949833722f44
VillePar <partsi1987@gmail.com>
2022-12-17 21:22:09 +0200

    DnD päivitys
    Vaihdettu DnD-kirjasto beautiful-dnd:ksi.

--- omahoksapp/src/omaLogo.js
+++ omahoksapp/src/omaLogo.js
@@ -2,7 +2,7 @@ import logo from './logo.png'
 
 const MainLogo = (props) => {
     return (
-      <div style={{backgroundColor:'#0112c7', position: 'absolute', top:-50, left: -1, right: -1}}>
+      <div style={{backgroundColor:'#b32d84', position: 'absolute', top:-50, left: -1, right: -1}}>
         <img src={logo} width={60} style={{transform:'translate(10px, 55px)'}}/>
         <h3 style={{color:'lightGrey', transform: 'translate(100px, 5px)'}}>{props.tutkinto} </h3>
       </div>
