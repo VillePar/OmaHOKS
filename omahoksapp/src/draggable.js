9efb07941dfa2c0e35802fec10d87b4e13f20afa
VillePar <partsi1987@gmail.com>
2022-12-18 21:22:05 +0200

    Parannuksia
    Nyt ei tarvi enää backendia taustalle pyörimään jotta appi toimii. Lisäksi saatu kaikki vedettävät opinnot jaettua fiksusti väliotsikoineen. Eriytetty raahattavien komponenttien koodi omaksi komponentiksi nimeltä draggable.js

---
+++ omahoksapp/src/draggable.js
@@ -0,0 +1,153 @@
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+
+const StudiesToDrag = ({list1, list2, list3, list4, list5, onDragEnd}) => {
+  
+  return (
+    
+  <div style={{overflowY:'scroll', overflowX: 'hidden', border: '2px solid black',width: '18%' ,position: 'absolute', top: 100, bottom: 2, left: 2 }}>
+    <h6 style={{textAlign: 'center', marginTop: 0, marginBottom: 0 }}>Ammatilliset pakolliset 55 op</h6>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Droppable droppableId='list'>
+        {(provided) => (
+          <div ref={provided.innerRef} {...provided.droppableProps} >
+            {list1.map((osa, index) => (
+              <Draggable key={osa.id} draggableId={osa.id.toString()} index={index} >
+                {(provided) => (
+                  <div 
+                  ref={provided.innerRef}
+                  {...provided.draggableProps}
+                  {...provided.dragHandleProps}>
+                  <div style={{width: '90%', height: '40px', backgroundColor: 'lightblue', margin:'5px', textAlign:'center', fontSize:'11px', border: '1px solid black'}}>
+                  {osa.name} {osa.points} op
+                  </div>
+            </div>
+              )}
+              </Draggable>
+            ))}
+        {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
+      <h6 style={{textAlign: 'center', marginTop: 0, marginBottom: 0 }}>Ammatilliset valinnaiset 90 op</h6>
+    </DragDropContext>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Droppable droppableId='list1'>
+        {(provided) => (
+          <div ref={provided.innerRef} {...provided.droppableProps} >
+            {list2.map((osa, index) => (
+              <Draggable key={osa.id} draggableId={osa.id.toString()} index={index} >
+                {(provided) => (
+                  <div 
+                  ref={provided.innerRef}
+                  {...provided.draggableProps}
+                  {...provided.dragHandleProps}>
+                  <div style={{width: '90%', height: '40px', backgroundColor: 'lightGreen', margin:'5px', textAlign:'center', fontSize:'11px', border: '1px solid black'}}>
+                  {osa.name} {osa.points} op
+                  </div>
+            </div>
+              )}
+              </Draggable>
+            ))}
+        {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
+    </DragDropContext>
+    <h6 style={{textAlign: 'center', marginTop: 0, marginBottom: 0 }}>Viestintä- ja vuorovaikutusosaaminen 11 op</h6>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Droppable droppableId='list2'>
+        {(provided) => (
+          <div ref={provided.innerRef} {...provided.droppableProps} >
+            {list3.map((osa) => {
+              
+              return(
+                osa.to_children.map((yto, index) => (
+                  <Draggable key={yto.id} draggableId={yto.id.toString()} index={index} >
+                {(provided) => (
+                  <div 
+                  ref={provided.innerRef}
+                  {...provided.draggableProps}
+                  {...provided.dragHandleProps}>
+                  <div style={{width: '90%', height: '40px', backgroundColor: 'lightYellow', margin:'5px', textAlign:'center', fontSize:'11px', border: '1px solid black'}}>
+                  {yto.name} {yto.points} op
+                  </div>
+            </div>
+              )}
+              </Draggable>
+
+                ))
+              )
+            }
+          )}
+        {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
+    </DragDropContext>
+    <h6 style={{textAlign: 'center', marginTop: 0, marginBottom: 0 }}>Matemaattis-luonnontieteellinen osaaminen 6 op</h6>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Droppable droppableId='list2'>
+        {(provided) => (
+          <div ref={provided.innerRef} {...provided.droppableProps} >
+            {list4.map((osa) => {
+              
+              return(
+                osa.to_children.map((yto, index) => (
+                  <Draggable key={yto.id} draggableId={yto.id.toString()} index={index} >
+                {(provided) => (
+                  <div 
+                  ref={provided.innerRef}
+                  {...provided.draggableProps}
+                  {...provided.dragHandleProps}>
+                  <div style={{width: '90%', height: '40px', backgroundColor: 'lightYellow', margin:'5px', textAlign:'center', fontSize:'11px', border: '1px solid black'}}>
+                  {yto.name} {yto.points} op
+                  </div>
+            </div>
+              )}
+              </Draggable>
+
+                ))
+              )
+            }
+          )}
+        {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
+    </DragDropContext>
+    <h6 style={{textAlign: 'center', marginTop: 0, marginBottom: 0 }}>Yhteiskunta- ja työelämäosaaminen 9 op</h6>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Droppable droppableId='list2'>
+        {(provided) => (
+          <div ref={provided.innerRef} {...provided.droppableProps} >
+            {list5.map((osa) => {
+              
+              return(
+                osa.to_children.map((yto, index) => (
+                  <Draggable key={yto.id} draggableId={yto.id.toString()} index={index} >
+                {(provided) => (
+                  <div 
+                  ref={provided.innerRef}
+                  {...provided.draggableProps}
+                  {...provided.dragHandleProps}>
+                  <div style={{width: '90%', height: '40px', backgroundColor: 'lightYellow', margin:'5px', textAlign:'center', fontSize:'11px', border: '1px solid black'}}>
+                  {yto.name} {yto.points} op
+                  </div>
+            </div>
+              )}
+              </Draggable>
+
+                ))
+              )
+            }
+          )}
+        {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
+    </DragDropContext>
+    </div>
+  )
+}
+
+export default StudiesToDrag;
