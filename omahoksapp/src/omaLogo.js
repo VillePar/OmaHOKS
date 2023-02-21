9eec921b7876d77d2df3e82eaf7b92bf8f21a1d2
VillePar <partsi1987@gmail.com>
2023-02-21 12:38:11 +0200

    Koodin siivomista yms.
    Siivottu koodia, lisätty kommentteja koodiin. Isoimpana uudistuksena tehty cssStyles-kansio ja sinne muutama css-tyylitiedosto. Korvattu aiemmin koodissa ollut inline-tyylittety kutsumalla luokkia css-tyylitiedostoista.

--- omahoksapp/src/omaLogo.js
+++ omahoksapp/src/omaLogo.js
@@ -1,4 +1,5 @@
 import logo from './logo.png'
+import './cssStyles/logoStyle.css'
 
 // function to update the sum of points in all of the periods
 const AllPoints = (props) => {
@@ -17,11 +18,12 @@ const AllPoints = (props) => {
        )
     }
 
-  
+// function to draw a bar for the upper edge of the screen, containing "Omahoks" logo, the name of the vocational qualification,
+// sum of all the points of the studies that user has dropped in periods and button to save all the data in PDF-form
 const MainLogo = (props) => {
     return (
-      <div style={{backgroundColor:'#b32d84', position: 'absolute', top:-60, left: 0, right: 0}}>
-        <button style={{width: '100px',border: '2px solid black',borderRadius: '5px',color:'#DCDCDC',backgroundColor: '#008080',transform: "translate(600px, 50px)"}}>Tallenna PDF</button>
+      <div className='upperPinkBar'>
+        <button className='pdfButton' style={{transform: "translate(600px, 50px)"}}>Tallenna PDF</button>
         
         <img src={logo} width={60} style={{transform:'translate(-80px, 60px)'}}/>
         <h4 style={{color:'lightGrey', transform: 'translate(90px, 10px)'}}>{props.tutkinto} {AllPoints(props.allPoints)}/{props.fromTotal} opintopistettä</h4>
@@ -30,6 +32,7 @@ const MainLogo = (props) => {
     )
   }
 
+// function to draw the "OmaHoks" logo when user is holding the mobile device in portrait mode.
 const PortraitLogo = () => {
   return (
     <img src={logo} width={180} height={120} style={{margin: 100}}/>
