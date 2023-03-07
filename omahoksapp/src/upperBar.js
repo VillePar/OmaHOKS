645667088664cd5f3edaf9f7c5178c3e22a1d603
VillePar <partsi1987@gmail.com>
2023-03-07 22:52:34 +0200

    Bugien korjausta
    Löytyi pari bugia. Aloitusajankohtanappi vaihtoi aktiivista tilaansa vaikka valinnan olisi perunut, korjattu siirtämällä valintavaihtoehto pääohjelmasta napin renderöivään komponenttiin. Local storage tallennus ei toiminut useEffect hookin kanssa joka päivitti valinnan ruudulle reaaliajassa. Lisätty useRef boolean jota if-lauseella tarkastellaan kyseisen useEffectin sistältä, tämä korjasi ongelman.

--- omahoksapp/src/upperBar.js
+++ omahoksapp/src/upperBar.js
@@ -8,7 +8,7 @@ const AllPoints = (props) => {
   const summa = []
   Object.entries(props).map(([columnId, column]) => {
     column.items.map((tutkinto) => {
-      if (column.orderNum !== 0){
+      if (column.orderNum !== 0 && column.orderNum !== 20){
         return(
           summa.push(tutkinto.points)
           )
@@ -18,47 +18,41 @@ const AllPoints = (props) => {
     return (
       summa.reduce((acc, currentValue) => acc + currentValue, 0)
       )
-    }
+    };
     
-
-
 // function to draw a bar for the upper edge of the screen, containing "Omahoks" logo, the name of the vocational qualification,
 // sum of all the points of the studies that user has dropped in periods and button to save all the data in PDF-form
 const MainLogo = (props) => {
 
-
   // "active" useState is for to determine active index of the seasonButtons 
-const [active, setActive] = useState(1)
+const [activeButton, setActiveButton] = useState(1)
 const toggleActive = (index) => {
-  setActive(index)
-}
+  setActiveButton(index)
+};
 
 // Two useEffect hooks to save and fetch the active button data from local storage
 useEffect(() => {
   const buttonData = localStorage.getItem("activeButton")
   if(buttonData){
-    setActive(JSON.parse(buttonData))
+    setActiveButton(JSON.parse(buttonData))
   }
-  else(
-    localStorage.setItem("activeButton", JSON.stringify(''))
-  )
-}, [])
+}, []);
 
 useEffect(() => {
-  localStorage.setItem("activeButton", JSON.stringify(active))
-},[])
-console.log(active)
+  localStorage.setItem("activeButton", JSON.stringify(activeButton))
+},[activeButton]);
+
     return (
       <div className='upperPinkBar'>
-        <button className={active === 1 ? 'seasonButtonActive': 'seasonButtonDeactive'} onClick={() => [toggleActive(1), props.autumn()]} style={{transform: "translate(500px, 100px)"}}>Syksy</button>
-        <button className={active === 2 ? 'seasonButtonActive': 'seasonButtonDeactive'} onClick={() => [toggleActive(2), props.spring()]} style={{transform: "translate(500px, 100px)"}}>Kevät</button>
+        <button className={activeButton === 1 ? 'seasonButtonActive': 'seasonButtonDeactive'} onClick={() => {if(window.confirm('Haluatko vaihtaa opintojen aloituksen syksylle?'))return[toggleActive(1), props.autumn()]}} style={{transform: "translate(500px, 100px)"}}>Syksy</button>
+        <button className={activeButton === 2 ? 'seasonButtonActive': 'seasonButtonDeactive'} onClick={() => {if(window.confirm('Haluatko vaihtaa opintojen aloituksen keväälle?'))return[toggleActive(2), props.spring()]}} style={{transform: "translate(500px, 100px)"}}>Kevät</button>
         <button className='pdfButton' style={{transform: "translate(550px, 95px)"}}>Tallenna PDF</button>
         <img src={logo} width={60} style={{transform:'translate(-240px, 108px)'}}/>
         <h4 style={{color:'lightGrey', transform: 'translate(75px, 50px)'}}>{props.qualification} {AllPoints(props.allPoints)}/{props.fromTotal} opintopistettä</h4>
         <h6 style={{color: 'lightGrey', transform: 'translate(525px, -5px)'}}>Opintojen aloitus:</h6>
         </div>
         )
-      }
+      };
 
 // function to draw the "OmaHoks" logo and notification to turn the mobile device when user is holding the mobile device in portrait mode.
 const PortraitScreen = () => {
@@ -72,6 +66,6 @@ const PortraitScreen = () => {
         </div>
       </div>
     )
-  }
+  };
 
-export  {MainLogo, PortraitScreen}
+export  {MainLogo, PortraitScreen};
